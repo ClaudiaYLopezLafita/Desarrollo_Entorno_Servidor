28d6425a3ab4fa53e94ee87993a7cd4939522dbb
ClaudiaYLopezLafita <claudiayll94@gmail.com>
2022-10-13 17:36:37 +0200

    format date

--- Unidad 2: JavaSript/Actividades_clase/Formulario y validaciones/js/ej3.js
+++ Unidad 2: JavaSript/Actividades_clase/Formulario y validaciones/js/ej3.js
@@ -28,12 +28,18 @@ function handleSubmit(e){
                 '\n - Debe estar relleno'+
                 '\n - Debe estar compuesto por 1 palabra minimo y 4 máxima');
         } else{
-            if(fecha==false || isValidFecha(fecha)){
+            if(fecha==false || isValidFecha(fecha)==false){
                 alert('El nombre debe ser un valor válido :'+
                 '\n - Debe estar relleno'+
                 '\n - Conformato: dd/mm/yyyy');
             } else{
-                alert('algo falla')
+                if(email==false || isValidEmail(email)==false){
+                    alert('La fecha debe ser un valor válido :'+
+                    '\n -Debe estar relleno'+
+                    '\n -Debe tener siguiente formato: xxxxxx@yyyyy.zzz');
+                } else{ 
+                    alert('o')
+                }
             }
         }
     }
@@ -80,15 +86,32 @@ function isValidText(text){
 
 function isValidFecha(fecha){
     const validacion = /^(((0[1-9]|[12][0-9]|3[01])[- /.](0[13578]|1[02])|(0[1-9]|[12][0-9]|30)[- /.](0[469]|11)|(0[1-9]|1\d|2[0-8])[- /.]02)[- /.]\d{4}|29[- /.]02[- /.](\d{2}(0[48]|[2468][048]|[13579][26])|([02468][048]|[1359][26])00))$$/;
-    return validacion.test(fecha);
+
+    var texto = fecha;
+    var salida = formato(texto);
+
+    return validacion.test(salida);
+
 }
-/*
+
+/**
+ * Convierte un texto de la forma 2017-01-10 a la forma
+ * 10/01/2017
+ *
+ * @param {string} texto Texto de la forma 2017-01-10
+ * @return {string} texto de la forma 10/01/2017
+ *
+ */
+ function formato(texto){
+    return texto.replace(/^(\d{4})-(\d{2})-(\d{2})$/g,'$3/$2/$1');
+}
+
 function isValidEmail(email){
 
     const validacion = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/;
     return validacion.test(email);
 }
-
+/*
 function isValidWeb(web){
     const validacion=/[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/
     return validacion.test(web);
